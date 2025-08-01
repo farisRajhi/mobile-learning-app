69ee7b2c92870855770533e236605d13a841e1e8
farisRajhi <136645428+farisRajhi@users.noreply.github.com>
2025-08-01 18:27:06 +0300

    faris

---
+++ learning/app/config/index.js
@@ -0,0 +1,2 @@
+import { REACT_APP_API_URL } from '@env';
+export const API_BASE_URL  = REACT_APP_API_URL;
---
+++ learning/app/services/api.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+
+export default axios.create({ baseURL: API_BASE_URL, timeout: 10000 });
